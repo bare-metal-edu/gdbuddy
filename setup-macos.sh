2716d935ca99eb28d1e98956640e265cccba0223
Coltin Colucci <Colucci01@allegheny.edu>
2025-09-25 23:18:40 -0400

    More dependencies installed

--- setup-macos.sh
+++ setup-macos.sh
@@ -12,8 +12,27 @@ if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Update Homebrew
+brew update
+
 # Install system dependencies
-brew install openocd gcc-arm-none-eabi python jq
+brew install \
+    openocd \
+    gcc-arm-none-eabi \
+    python \
+    jq \
+    cmake \
+    curl \
+    git \
+    automake \
+    autoconf \
+    libtool \
+    libftdi \
+    libusb \
+    pkg-config \
+    pcre \
+    gdb \
+    wget \
 
 # (Optional) Install Python packages globally or recommend a venv/uv step
 pip3 install --user gatorgrade
